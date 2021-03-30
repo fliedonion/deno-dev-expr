bcc79044ba8c9ed676e237190d089fd6ef5548a6
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2021-03-30 12:46:57 +0900

    add not found and inline response empty uri

--- exapmles/firstscripts/mod.js
+++ exapmles/firstscripts/mod.js
@@ -1,7 +1,13 @@
 import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
 import baseLayout from "./baselayout.jsx";
+import notFound from "./notfound.jsx";
 
 serve({
   "/": baseLayout,
-  404: "notFoundPage",
+  "/empty": () => new Response(`<html>
+    <head><title>Empty</title></head>
+    <body>
+    </body>
+    </html>`, {headers: {"content-type": "text/html; charset=utf-8"}}),
+  404: notFound,
 });
---
+++ exapmles/firstscripts/notfound.jsx
@@ -0,0 +1,14 @@
+export default function NotFoundPage({ children }) {
+  return (<html lang="ja">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+      />
+    </head>
+    <body>
+      ４０４ のっと ファウンド
+    </body>
+  </html>);
+}
