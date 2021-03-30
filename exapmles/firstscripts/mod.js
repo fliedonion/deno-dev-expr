6b1115bf28ef18b285cd0e2b31e1f52958937f60
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2021-03-31 02:10:17 +0900

    replace 404

--- exapmles/firstscripts/mod.js
+++ exapmles/firstscripts/mod.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
 import index from "./pages/index.jsx";
-import notFound from "./notfound.jsx";
+import error404 from "./pages/error404.jsx";
 
 serve({
   "/": index,
@@ -9,5 +9,5 @@ serve({
     <body>
     </body>
     </html>`, {headers: {"content-type": "text/html; charset=utf-8"}}),
-  404: notFound,
+  404: error404,
 });
--- exapmles/firstscripts/pages/index.jsx
+++ exapmles/firstscripts/pages/index.jsx
@@ -2,8 +2,6 @@ import { h } from "https://deno.land/x/sift@0.1.6/mod.ts";
 import BaseLayout from "../components/baselayout.jsx";
 
 export default function indexPage(request) {
-  const { params } = new URL(request.url);
-  const v = params.get("value");
 
   return (<BaseLayout>
     Index Page { v }
