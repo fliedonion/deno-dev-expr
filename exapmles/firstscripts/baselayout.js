ae261020c8faf00006668e0f015c77bd9518cf24
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2021-03-30 12:39:19 +0900

    Expr Using jsx

---
+++ exapmles/firstscripts/baselayout.js
@@ -0,0 +1,14 @@
+export default function BaseLayout({ children }) {
+  return (<html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+      />
+    </head>
+    <body>
+      Hello Layout
+    </body>
+  </html>);
+}
